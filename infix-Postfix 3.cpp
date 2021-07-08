af6dba37ea7e23ffc287de6ba1ea68c2a78861f0
akbarananto12 <83356837+akbarananto12@users.noreply.github.com>
2021-07-08 21:04:00 +0700

    Update infix-Postfix 3.cpp

--- infix-Postfix 3.cpp
+++ infix-Postfix 3.cpp
@@ -1,1 +1,51 @@
+/*
+ANGGOTA KELOMPOK
+Ananto Akbar               2017051069
+Assyfa Naziwa Ganandy      2017051046
+Karina Adityas Ramadhanti  2017051041
+*/
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+
+bool operand(char a){
+int asci = (int)a;
+if (asci >= 48 && asci <= 57){
+    return true;
+} else {return false;}
+
+}
+
+int prioritas(char c) {
+    if(c == '^'||c == '%')
+        return 3;
+    else if(c == '/')
+        return 2;
+    else if(c=='*')
+         return 1;
+    else if(c == '+' || c == '-')
+        return -1;
+    else
+        return -2;
+}
+
+bool is_operator(char a){
+int asci = (int)a;
+if (!(asci >= 48 && asci <= 57)){
+    return true;
+} else {return false;}
+
+}
+
+bool is_operator2(char a){
+if (a == '/' ||a == '*' ||a == '-' || a == '+' || a == '%'){
+    return true;
+} else {return false;}
+
+
+}
+
+#KARINA 1
 
